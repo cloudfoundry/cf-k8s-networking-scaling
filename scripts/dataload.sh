ad3c87d465ba92ab9f20a0d4cf3c7d11b69c89fb
Alex Standke <astandke@pivotal.io>
2019-11-07 21:44:51 +0000

    make number of connections configurable

--- scripts/dataload.sh
+++ scripts/dataload.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 source ../scripts/utils.sh
+source ../vars.sh
 
 apib -T https://astandke.com
 
 echo "latency" > rawlatencies.txt
 
 while true; do
-  apib -N $(udate) -c 10 -d 5 -S $1
+  apib -N $(udate) -c $DATAPLANE_NUM_CONNECTIONS -d 5 -S $1
 done
