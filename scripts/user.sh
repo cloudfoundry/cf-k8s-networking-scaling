859551f6170c8d1a614d98a7a87ddc649a16cb8f
Leah Hanson <lhanson@pivotal.io>
2019-11-19 01:01:26 +0000

    Log failures, work for namespaces

--- scripts/user.sh
+++ scripts/user.sh
@@ -4,16 +4,18 @@ source ../vars.sh
 source ../scripts/utils.sh
 
 echo "$(udate),$1,STARTED"
+TARGET_URL=httpbin-$1.example.com
 
 if [ "$NAMESPACES" = "1" ]; then
-  kubetpl render ../yaml/namespace/httpbin-gateway.yaml -s NAME=httpbin-$1 -s NAMESPACE=ns-0 | kubectl apply -f -
-  kubetpl render ../yaml/namespace/httpbin-virtualservice.yaml -s NAME=httpbin-$1 -s NAMESPACE=ns-0 | kubectl apply -f -
+  TARGET_URL=httpbin-0-$1.example.com
+  kubetpl render ../yaml/namespace/httpbin-gateway.yaml -s NAME=httpbin-0-$1 -s NAMESPACE=ns-0 | kubectl apply -f -
+  kubetpl render ../yaml/namespace/httpbin-virtualservice.yaml -s NAME=httpbin-0-$1 -s NAMESPACE=ns-0 | kubectl apply -f -
 else
   kubetpl render ../yaml/httpbin-gateway.yaml -s NAME=httpbin-$1 -s NAMESPACE=default | kubectl apply -f -
   kubetpl render ../yaml/httpbin-virtualservice.yaml -s NAME=httpbin-$1 -s NAMESPACE=default | kubectl apply -f -
 fi
 
-until [ $(curl -s -o /dev/null -w "%{http_code}" -HHost:httpbin-$1.example.com http://$INGRESS_HOST:$INGRESS_PORT/status/200) -eq 200 ]; do true; done
+until [ $(curl -s -o /dev/null -w "%{http_code}" -HHost:$TARGET_URL http://$INGRESS_HOST:$INGRESS_PORT/status/200) -eq 200 ]; do true; done
 
 echo "$(udate),$1,SUCCESS"
 >&2 wlog "SUCCESS $1"
@@ -22,8 +24,9 @@ lastfail=$(udate)
 secondsToWait=720
 for ((i=$secondsToWait; i>0; i--)); do
   sleep 1 &
-  if [ $(curl -s -o /dev/null -w "%{http_code}" -HHost:httpbin-$1.example.com http://$INGRESS_HOST:$INGRESS_PORT/status/200) -ne 200 ]; then
+  if [ $(curl -s -o /dev/null -w "%{http_code}" -HHost:$TARGET_URL http://$INGRESS_HOST:$INGRESS_PORT/status/200) -ne 200 ]; then
     lastfail=$(udate)
+    echo "$(udate),$1,FAILURE"
   fi
   wait
 done
