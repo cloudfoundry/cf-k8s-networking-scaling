ec3a3aeed1bdefb59ec6304cc63712155c73f2d9
Leah Hanson <lhanson@pivotal.io>
2019-11-22 23:18:30 +0000

    Add first-draft of script for pulling a metric from prometheus

---
+++ scripts/prometheus_pilot_convergence.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+1>&2 1>/dev/null kubectl port-forward -n istio-system service/prometheus 9090 &
+KPF_PID=$!
+sleep 2 # let the portforward start working
+
+JSON=$(2>/dev/null curl 'http://localhost:9090/api/v1/query?query=pilot_proxy_convergence_time_bucket[1m]')
+echo "$JSON" | jq -r '["bucket", "timestamp", "value"], (.data.result[] | [.metric.le] + .values[]) | @csv'
+
+
+kill $KPF_PID # gotta clean up
