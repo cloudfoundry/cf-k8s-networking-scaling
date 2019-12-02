6f20e38bf0fbd46f871ade5817ae41503926f310
Alex Standke <astandke@pivotal.io>
2019-12-02 22:30:31 +0000

    auto generate prometheus query step

--- scripts/prometheus_data.sh
+++ scripts/prometheus_data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-start=$1
-
 echo "Exposing prometheus for data collection"
 
 # expose prometheus through istio's ingress gateway
@@ -54,53 +52,61 @@ spec:
 ---
 EOF
 
-sleep 5 # let the portforward start working
+# sleep 5 # let the portforward start working
 
 INGRESS_IP=$(kubectl get svc -n istio-system istio-ingressgateway | awk 'NR>1 {print $4}')
 START=$1
 END=$(date +%s)
 
+STEP=$((($END-$START)/1000))
+STEP=$(( $STEP < 15 ? 15 : $STEP))
+
+echo "Step size: $STEP"
+
 queryprom ()
 {
-  shift
-
-  curl -s -G \
+  data=$(curl -s -G \
     --data-urlencode "query=$@" \
     --data-urlencode "start=$START" \
     --data-urlencode "end=$END" \
-    --data-urlencode "step=15" \
-    http://$INGRESS_IP:15030/api/v1/query_range
-}
+    --data-urlencode "step=$STEP" \
+    http://$INGRESS_IP:15030/api/v1/query_range)
 
+  if [ "$(echo $data | jq -r '.status')" == "error" ]; then
+    >&2 echo "Could not query! $data"
+  fi
+
+  echo $data
+}
 
 echo "Querying prometheus"
 
 # CPU usage per node
-queryprom $start '(100 - (sum by (instance) (irate(node_cpu_seconds_total{mode="idle"}[24h])) * 12.5)) * on(instance) group_left(nodename) node_uname_info' |
+queryprom '(100 - (sum by (instance) (irate(node_cpu_seconds_total{mode="idle"}[24h])) * 12.5)) * on(instance) group_left(nodename) node_uname_info' |
    jq -r '["timestamp", "percent", "nodename", "type"], (.data.result[] | (.values[] | [((.[0]|tostring) + "000000000"|tonumber), (.[1]|tonumber)]) + [.metric.nodename] + ["cpu"]) | @csv' > nodemon.csv
 
 # Memory usage per node
-queryprom $start '100 - (node_memory_MemFree_bytes / node_memory_MemTotal_bytes * 100)* on(instance) group_left(nodename) node_uname_info' |
+queryprom '100 - (node_memory_MemFree_bytes / node_memory_MemTotal_bytes * 100)* on(instance) group_left(nodename) node_uname_info' |
    jq -r '(.data.result[] | (.values[] | [((.[0]|tostring) + "000000000"|tonumber), (.[1]|tonumber)]) + [.metric.nodename] + ["memory"]) | @csv' >> nodemon.csv
 
 # Number of pilots
-queryprom $start "sum(pilot_info)" | \
+queryprom "sum(pilot_info)" | \
   jq -r '["stamp", "count"], (.data.result[] | .values[] | [((.[0]|tostring) + "000000000"|tonumber), (.[1]|tonumber)]) | @csv' > howmanypilots.csv
 
 # Gateway memory usage
-queryprom $start 'envoy_server_memory_allocated{app="istio-ingressgateway"}' | \
+queryprom 'envoy_server_memory_allocated{app="istio-ingressgateway"}' | \
   jq -r '["timestamp","memory","podname"], (.data.result[] | (.values[] | [((.[0]|tostring) + "000000000"|tonumber), (.[1]|tonumber)]) + [.metric.pod_name]) | @csv' > gatewaystats.csv
 
 # Sidecar memory usage
-queryprom $start 'envoy_server_memory_allocated{app=~"httpbin.*"}' | \
+queryprom 'envoy_server_memory_allocated{app=~"httpbin.*"}' | \
   jq -r '["timestamp","memory","podname"], (.data.result[] | (.values[] | [((.[0]|tostring) + "000000000"|tonumber), (.[1]|tonumber)]) + [.metric.pod_name]) | @csv' > sidecarstats.csv
 
 # Proxy convergence
-queryprom $start "pilot_proxy_convergence_time_bucket" | \
+queryprom "pilot_proxy_convergence_time_bucket" | \
   jq -r '["timestamp","value","bucket"], (.data.result[] | (.values[] | [((.[0]|tostring) + "000000000"|tonumber), (.[1]|tonumber)]) + [.metric.le]) | @csv' > proxy_convergence.csv
 
 # Mapping of nodes to pods
-queryprom $start 'sum(container_tasks_state{pod_name!=""}) by (instance,pod_name)' | \
+queryprom 'sum(container_tasks_state{pod_name!=""}) by (instance,pod_name)' | \
    jq -r '["stamp","node","pod"], (.data.result[] | (.values[] | [((.[0]|tostring) + "000000000"|tonumber)]) + [.metric.instance, .metric.pod_name]) | @csv' > nodes4pods.csv
 
 echo "Prometheus data collected"
