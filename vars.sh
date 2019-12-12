a42e163f8940ad9945dadd8e8cdc66cbaa5f1017
Leah Hanson <lhanson@pivotal.io>
2019-12-12 17:14:42 +0000

    Labels for sidecar workload selector need to be on pods.
    - Add sidecargroup label to pods deployed by httpbin deployments
    - Bump from 1.4.0 to 1.4.2 (the security patch)
    - Improve documentation comments in vars.sh to match new script
    behavior.

--- vars.sh
+++ vars.sh
@@ -4,16 +4,16 @@ MACHINE_TYPE=n1-standard-8
 AVAILABILITY_ZONE=$(gcloud compute instances list | grep "$(hostname) " | awk '{print $2}')
 DATAPLANE_NUM_CONNECTIONS=10
 
-ISTIO_FOLDER=/home/pivotal/istio-1.4.0
+ISTIO_FOLDER=/home/pivotal/istio-1.4.2
 ISTIO_TAINT=1
 NODES_FOR_ISTIO=20
 
 MIXERLESS_TELEMETRY=0
 
-NUM_APPS=1000 # must be equal to or larger than NUM_USERS
+NUM_APPS=1000 # NUM_APPS >= NUM_USERS * NUM_GROUPS && NUM_APPS % NUM_GROUPS == 0
 NUM_USERS=100
-USER_DELAY=10
+USER_DELAY=10 # in seconds
 
-NAMESPACES=0 # NUM_APPS will be overridden when this is true (with 1000)
+NAMESPACES=0 # if 0, groups within the default namespace will be used
+NUM_GROUPS=10 # set to 1 for everything in one group or namespace
 
-ISOLATE_DATAPLANE=0
