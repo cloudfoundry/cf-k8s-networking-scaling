19cd51033d62757facd605550be0e452c446aab7
Alex Standke <astandke@pivotal.io>
2020-01-22 00:39:49 +0000

    make it possible for NUM_USERS to be greater than the number of apps in a group

--- scripts/user.sh
+++ scripts/user.sh
@@ -3,18 +3,18 @@
 source ../vars.sh
 source ../scripts/utils.sh
 
-echo "$(udate),$1,STARTED,"
-TARGET_URL=httpbin-$1-g0.example.com
+echo "$(udate),$1,$2,STARTED,"
+TARGET_URL=httpbin-$1-g$2.example.com
 
-if [ $NAMESPACES -eq 1 ]; then namespace=ns-0; else namespace=default; fi
+if [ $NAMESPACES -eq 1 ]; then namespace=ns-$2; else namespace=default; fi
 
-# kubetpl render ../yaml/service.yaml -s NAME=httpbin-$1-g0 -s NAMESPACE=$namespace -s GROUP=0 | kubectl apply -f -
-kubetpl render ../yaml/gateway.yaml ../yaml/virtualservice.yaml -s NAME=httpbin-$1-g0 -s NAMESPACE=$namespace | kubectl apply -f -
+# kubetpl render ../yaml/service.yaml -s NAME=httpbin-$1-g$2 -s NAMESPACE=$namespace -s GROUP=0 | kubectl apply -f -
+kubetpl render ../yaml/gateway.yaml ../yaml/virtualservice.yaml -s NAME=httpbin-$1-g$2 -s NAMESPACE=$namespace | kubectl apply -f -
 
 until [ $(curl -s -o /dev/null -w "%{http_code}" -HHost:$TARGET_URL http://$INGRESS_HOST:$INGRESS_PORT/status/200) -eq 200 ]; do true; done
 
-echo "$(udate),$1,SUCCESS,"
->&2 wlog "SUCCESS $1"
+echo "$(udate),$1,$2,SUCCESS,"
+>&2 wlog "SUCCESS $1-g$2"
 
 lastfail=$(udate)
 secondsToWait=120
@@ -23,11 +23,11 @@ for ((i=$secondsToWait; i>0; i--)); do
   status=$(curl -s -o /dev/null -w "%{http_code}" -HHost:$TARGET_URL http://$INGRESS_HOST:$INGRESS_PORT/status/200)
   if [ $status -ne 200 ]; then
     lastfail=$(udate)
-    echo "$(udate),$1,FAILURE,$status"
+    echo "$(udate),$1,$2,FAILURE,$status"
   fi
   wait
 done
 
-echo "$lastfail,$1,COMPLETED,"
->&2 wlog "Completed $1"
+echo "$lastfail,$1,$2,COMPLETED,"
+>&2 wlog "Completed $1-g$2"
 
--- scripts/userfactory.sh
+++ scripts/userfactory.sh
@@ -3,12 +3,14 @@
 source ../vars.sh
 source ../scripts/utils.sh
 
-echo "stamp,usernum,event,status"
+echo "stamp,usernum,groupnum,event,status"
 
-for ((n=0;n<$NUM_USERS;n++))
-do
-  ./../scripts/user.sh $n &
-  sleep $USER_DELAY
+group_size=$((NUM_APPS / NUM_GROUPS))
+
+for ((group = 0 ; group < $NUM_GROUPS ; group++)); do
+  for ((count = 0; count < $group_size; count++)); do
+    ./../scripts/user.sh $count $group &
+  done
 done
 
 >&2 wlog "user factory closing"
