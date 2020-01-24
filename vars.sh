bc83c1b8e8e0cdaa29530c1b3131857d4dd795a7
Alex Standke <astandke@pivotal.io>
2020-01-24 17:55:08 +0000

    200 group ga scale config

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 CLUSTER_VERSION=1.14.8-gke.12
-NUM_NODES=70
+NUM_NODES=100
 MACHINE_TYPE=n1-standard-8
 AVAILABILITY_ZONE=$(gcloud compute instances list | grep "$(hostname) " | awk '{print $2}')
 DATAPLANE_NUM_CONNECTIONS=10
@@ -14,6 +14,6 @@ NUM_APPS=2000 # NUM_APPS >= NUM_USERS * NUM_GROUPS && NUM_APPS % NUM_GROUPS == 0
 NUM_USERS=1000
 USER_DELAY=10 # in seconds
 
-NAMESPACES=1 # if 0, groups within the default namespace will be used
-NUM_GROUPS=1 # set to 1 for everything in one group or namespace
+NAMESPACES=0 # if 0, groups within the default namespace will be used
+NUM_GROUPS=200 # set to 1 for everything in one group or namespace
 
