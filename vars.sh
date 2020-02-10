7993e6ac7436eebce74ec17b7f7bc7bfd355a569
Alex Standke <astandke@pivotal.io>
2020-02-10 22:20:28 +0000

    gabe experiments

--- vars.sh
+++ vars.sh
@@ -10,13 +10,13 @@ NODES_FOR_ISTIO=20
 
 MIXERLESS_TELEMETRY=0
 
-NUM_APPS=2000 # NUM_APPS >= NUM_USERS * NUM_GROUPS && NUM_APPS % NUM_GROUPS == 0
-NUM_USERS=1000
+NUM_APPS=10 # NUM_APPS >= NUM_USERS * NUM_GROUPS && NUM_APPS % NUM_GROUPS == 0
+NUM_USERS=10
 USER_DELAY=1 # in seconds
 USER_POLL_DELAY=3 # how often to poll for upness of a route, 1 is fine for USER_DELAY > 10
 
 NAMESPACES=0 # if 0, groups within the default namespace will be used
-NUM_GROUPS=200 # set to 1 for everything in one group or namespace
+NUM_GROUPS=1 # set to 1 for everything in one group or namespace
 
 # steady state experiment where the number of virtualservices and gateways (routes) is constant
 #   through the test, every time a user creates a route they also delete another one
