823f112fb63ddecf1bf96bc5915b2e15eb63b25d
Leah Hanson <lhanson@pivotal.io>
2020-03-09 16:31:43 +0000

    Update to do steady state
    - Changes cluster version to something that currently works.
    - Updates comments for waht the variables do.
    - NUM_USERS < NUM_APPS / 2

--- envoy-scaling-experiment/vars.sh
+++ envoy-scaling-experiment/vars.sh
@@ -1,14 +1,12 @@
-CLUSTER_VERSION=1.14.8-gke.33
-NUM_NODES=2
+CLUSTER_VERSION=1.14.10-gke.24
+NUM_NODES=100
 MACHINE_TYPE=n1-standard-8
 
-DATAPLANE_NUM_CONNECTIONS=10
-
-NUM_APPS=20 # NUM_APPS >= NUM_USERS * NUM_GROUPS && NUM_APPS % NUM_GROUPS == 0
-NUM_USERS=$NUM_APPS
-USER_DELAY=1 # in seconds
-USER_POLL_DELAY=3 # how often to poll for upness of a route, 1 is fine for USER_DELAY > 10
+NUM_APPS=2000 # NUM_APPS >= NUM_USERS * NUM_GROUPS && NUM_APPS % NUM_GROUPS == 0
+NUM_USERS=100 # number of routes to create during experiment
+USER_DELAY=1 # delay between route creations, in seconds
+USER_POLL_DELAY=2 # will poll all routes in parallel, then wait this many seconds, then poll again
 
 # steady state experiment where the number of virtualservices and gateways (routes) is constant
 #   through the test, every time a user creates a route they also delete another one
-STEADY_STATE=0
+STEADY_STATE=1
