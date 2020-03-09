bfc6e30fce66603ebc0de6df26804ff72da67b9a
Leah Hanson <lhanson@pivotal.io>
2020-03-09 21:11:30 +0000

    Make route poller only use half the CPUs.
    When it gets to use all 16 CPUs on the client, the machine is nearly fully utilized before we even start ending CP load. I'm worried that if we add this much load on the client side, we might end up sending config more slowly than desired and not loading Envoy as heavily on the CP side.
    
    Signed-off-by: Mikael Manukyan <mmanukyan@pivotal.io>

--- envoy-scaling-experiment/scripts/route-poller.sh
+++ envoy-scaling-experiment/scripts/route-poller.sh
@@ -5,7 +5,7 @@ source ../scripts/utils.sh
 
 mkdir curlstuff
 while true; do
-  seq 0 $(($NUM_APPS - 1)) | xargs -n 1 -P0 -I {} bash -c '
+  seq 0 $(($NUM_APPS - 1)) | xargs -n 1 -P8 -I {} bash -c '
     source ../scripts/utils.sh
     url="{}.example.com"
     status=$(curl -sS -w "%{http_code}" -H "Host:${url}" http://$INGRESS_IP:80/status/200 2>> curlstuff/route-{}.log)
