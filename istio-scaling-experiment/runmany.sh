8fa55e6099658877fba345dd5dacad18cec888a7
Mikael Manukyan <mmanukyan@pivotal.io>
2020-03-20 23:02:22 +0000

    Fix runmany.sh

--- istio-scaling-experiment/runmany.sh
+++ istio-scaling-experiment/runmany.sh
@@ -4,6 +4,6 @@ for filename in ./experiment_vars/*; do
   [ -e "$filename" ] || continue
   experiment=$(basename $filename)
   echo "Running: $experiment"
-  echo cp "$filename/*" .
-  echo ./run.sh "$experiment"
+  cp $filename/* .
+  ./run.sh "$experiment"
 done
