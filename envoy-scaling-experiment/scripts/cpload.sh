bd8182d358aa4e6f59898a614373c2f9d44b8820
Mikael Manukyan <mmanukyan@pivotal.io>
2020-04-20 21:27:22 +0000

    Update graphs, remove /config_dump polling, add poll delay
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- envoy-scaling-experiment/scripts/cpload.sh
+++ envoy-scaling-experiment/scripts/cpload.sh
@@ -6,27 +6,6 @@ source ../scripts/progressbar.sh
 
 mkdir -p curlstuff
 
-poll() {
-  local user="${1}"
-  local url="${user}.example.com"
-  local start="$(udate)"
-
-  local status=0
-
-  while [[ "${status}" != "200" ]]; do
-    status="$(curl -sS -w "%{http_code}" -H "Host:${url}" http://$INGRESS_IP:80/status/200 2>> curlstuff/route-${user}.log)"
-    echo "$(udate),${user},${status},${start}"
-  done
-}
-
-user() {
-  local user="${1}"
-  local navigator_port="${2}"
-  local routes="${3}"
-  set_routes "${2}" "$(seq -s',' 0 $i),$(seq -s',' $(($half_routes + $i)) $last_route)"
-  poll "${i}"
-}
-
 main() {
   local navigator_port="${1}"
   local last_route="${2}"
@@ -37,7 +16,7 @@ main() {
   let num_users_minus_one="${NUM_USERS} - 1"
   for (( i = 0; i < ${NUM_USERS}; i++ )); do
     draw_progress_bar "${i}" "${num_users_minus_one}" "users" >&2
-    user "${i}" "${navigator_port}" "$(seq -s',' 0 $i),$(seq -s',' $(($half_routes + $i)) $last_route)" &
+    ./../scripts/user.sh "${i}" "${navigator_port}" "$(seq -s',' 0 $i),$(seq -s',' $(($half_routes + $i)) $last_route)" &
     sleep $USER_DELAY
   done
 
---
+++ envoy-scaling-experiment/scripts/user.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ../vars.sh
+source ../scripts/utils.sh
+mkdir -p curlstuff
+
+poll() {
+  local user="${1}"
+  local url="${user}.example.com"
+  local start="$(udate)"
+
+  local status=0
+
+  while [[ "${status}" != "200" ]]; do
+    status="$(curl -sS -w "%{http_code}" -H "Host:${url}" http://$INGRESS_IP:80/status/200 2>> curlstuff/route-${user}.log)"
+    sleep ${USER_POLL_DELAY}
+    echo "$(udate),${user},${status},${start}"
+  done
+}
+
+user() {
+  local user="${1}"
+  local navigator_port="${2}"
+  local routes="${3}"
+
+  set_routes "${navigator_port}" "${routes}"
+  poll "${user}"
+}
+
+user "${@}"
